21f2c2c095d647459a4a92d132b3f95e35777029
Zach Evans <zqevans@gmail.com>
2023-06-23 02:18:27 +0000

    Remove relative position bias, add context mask

--- audio_diffusion_pytorch/modules.py
+++ audio_diffusion_pytorch/modules.py
@@ -279,57 +279,6 @@ class Unpatcher(nn.Module):
 """
 Attention Components
 """
-
-
-class RelativePositionBias(nn.Module):
-    def __init__(self, num_buckets: int, max_distance: int, num_heads: int):
-        super().__init__()
-        self.num_buckets = num_buckets
-        self.max_distance = max_distance
-        self.num_heads = num_heads
-        self.relative_attention_bias = nn.Embedding(num_buckets, num_heads)
-
-    @staticmethod
-    def _relative_position_bucket(
-        relative_position: Tensor, num_buckets: int, max_distance: int
-    ):
-        num_buckets //= 2
-        ret = (relative_position >= 0).to(torch.long) * num_buckets
-        n = torch.abs(relative_position)
-
-        max_exact = num_buckets // 2
-        is_small = n < max_exact
-
-        val_if_large = (
-            max_exact
-            + (
-                torch.log(n.float() / max_exact)
-                / log(max_distance / max_exact)
-                * (num_buckets - max_exact)
-            ).long()
-        )
-        val_if_large = torch.min(
-            val_if_large, torch.full_like(val_if_large, num_buckets - 1)
-        )
-
-        ret += torch.where(is_small, n, val_if_large)
-        return ret
-
-    def forward(self, num_queries: int, num_keys: int) -> Tensor:
-        i, j, device = num_queries, num_keys, self.relative_attention_bias.weight.device
-        q_pos = torch.arange(j - i, j, dtype=torch.long, device=device)
-        k_pos = torch.arange(j, dtype=torch.long, device=device)
-        rel_pos = rearrange(k_pos, "j -> 1 j") - rearrange(q_pos, "i -> i 1")
-
-        relative_position_bucket = self._relative_position_bucket(
-            rel_pos, num_buckets=self.num_buckets, max_distance=self.max_distance
-        )
-
-        bias = self.relative_attention_bias(relative_position_bucket)
-        bias = rearrange(bias, "m n h -> 1 h m n")
-        return bias
-
-
 def FeedForward(features: int, multiplier: int) -> nn.Module:
     mid_features = features * multiplier
     return nn.Sequential(
@@ -337,6 +286,16 @@ def FeedForward(features: int, multiplier: int) -> nn.Module:
         nn.GELU(),
         nn.Linear(in_features=mid_features, out_features=features),
     )
+
+def add_mask(sim: Tensor, mask: Tensor) -> Tensor:
+    b, ndim = sim.shape[0], mask.ndim
+    if ndim == 3:
+        mask = rearrange(mask, "b n m -> b 1 n m")
+    if ndim == 2:
+        mask = repeat(mask, "n m -> b 1 n m", b=b)
+    max_neg_value = -torch.finfo(sim.dtype).max
+    sim = sim.masked_fill(~mask, max_neg_value)
+    return sim
 
 
 class AttentionBase(nn.Module):
@@ -346,48 +305,42 @@ class AttentionBase(nn.Module):
         *,
         head_features: int,
         num_heads: int,
-        use_rel_pos: bool,
-        rel_pos_num_buckets: Optional[int] = None,
-        rel_pos_max_distance: Optional[int] = None,
+        out_features: Optional[int] = None,
     ):
         super().__init__()
-        self.scale = head_features ** -0.5
+        self.scale = head_features**-0.5
         self.num_heads = num_heads
-        self.use_rel_pos = use_rel_pos
         mid_features = head_features * num_heads
+        out_features = default(out_features, features)
 
-        if use_rel_pos:
-            assert exists(rel_pos_num_buckets) and exists(rel_pos_max_distance)
-            self.rel_pos = RelativePositionBias(
-                num_buckets=rel_pos_num_buckets,
-                max_distance=rel_pos_max_distance,
-                num_heads=num_heads,
-            )
+        self.to_out = nn.Linear(
+            in_features=mid_features, out_features=out_features, bias=False
+        )
 
-        self.use_flash = version.parse(torch.__version__) >= version.parse('2.0.0') and not use_rel_pos
+        self.use_flash = version.parse(torch.__version__) >= version.parse('2.0.0')
 
-        self.to_out = nn.Linear(in_features=mid_features, out_features=features)
-
-    def forward(self, q: Tensor, k: Tensor, v: Tensor) -> Tensor:
+    def forward(
+        self, q: Tensor, k: Tensor, v: Tensor, mask: Optional[Tensor] = None
+    ) -> Tensor:
         # Split heads
         q, k, v = rearrange_many((q, k, v), "b n (h d) -> b h n d", h=self.num_heads)
-        
+
         if not self.use_flash:
-            # Compute similarity matrix
-            sim = einsum("... n d, ... m d -> ... n m", q, k)
-            sim = (sim + self.rel_pos(*sim.shape[-2:])) if self.use_rel_pos else sim
-            sim = sim * self.scale
+            # Compute similarity matrix and add eventual mask
+            sim = einsum("... n d, ... m d -> ... n m", q, k) * self.scale
+            sim = add_mask(sim, mask) if exists(mask) else sim
+
             # Get attention matrix with softmax
-            attn = sim.softmax(dim=-1)
+            attn = sim.softmax(dim=-1, dtype=torch.float32)
+
             # Compute values
             out = einsum("... n m, ... m d -> ... n d", attn, v)
         else:
             with sdp_kernel(enable_flash = True, enable_math=False, enable_mem_efficient= False):
                 out = F.scaled_dot_product_attention(q, k, v, is_causal=False)
-        
+
         out = rearrange(out, "b h n d -> b n (h d)")
         return self.to_out(out)
-
 
 class Attention(nn.Module):
     def __init__(
@@ -396,13 +349,13 @@ class Attention(nn.Module):
         *,
         head_features: int,
         num_heads: int,
+        out_features: Optional[int] = None,
         context_features: Optional[int] = None,
-        use_rel_pos: bool,
-        rel_pos_num_buckets: Optional[int] = None,
-        rel_pos_max_distance: Optional[int] = None,
+        causal: bool = False,
     ):
         super().__init__()
         self.context_features = context_features
+        self.causal = causal
         mid_features = head_features * num_heads
         context_features = default(context_features, features)
 
@@ -418,22 +371,40 @@ class Attention(nn.Module):
             features,
             num_heads=num_heads,
             head_features=head_features,
-            use_rel_pos=use_rel_pos,
-            rel_pos_num_buckets=rel_pos_num_buckets,
-            rel_pos_max_distance=rel_pos_max_distance,
+            out_features=out_features,
         )
 
-    def forward(self, x: Tensor, *, context: Optional[Tensor] = None) -> Tensor:
+    def forward(
+        self,
+        x: Tensor, # [b, n, c]
+        context: Optional[Tensor] = None, # [b, m, d]
+        context_mask: Optional[Tensor] = None,  # [b, m], false is masked
+    ) -> Tensor:
         assert_message = "You must provide a context when using context_features"
         assert not self.context_features or exists(context), assert_message
         # Use context if provided
         context = default(context, x)
         # Normalize then compute q from input and k,v from context
         x, context = self.norm(x), self.norm_context(context)
+
         q, k, v = (self.to_q(x), *torch.chunk(self.to_kv(context), chunks=2, dim=-1))
+
+        if exists(context_mask):
+            # Mask out cross-attention for padding tokens
+            mask = repeat(context_mask, "b m -> b m d", d=v.shape[-1])
+            k, v = k * mask, v * mask
+
         # Compute and return attention
         return self.attention(q, k, v)
 
+
+def FeedForward(features: int, multiplier: int) -> nn.Module:
+    mid_features = features * multiplier
+    return nn.Sequential(
+        nn.Linear(in_features=features, out_features=mid_features),
+        nn.GELU(),
+        nn.Linear(in_features=mid_features, out_features=features),
+    )
 
 """
 Transformer Blocks
@@ -447,9 +418,6 @@ class TransformerBlock(nn.Module):
         num_heads: int,
         head_features: int,
         multiplier: int,
-        use_rel_pos: bool,
-        rel_pos_num_buckets: Optional[int] = None,
-        rel_pos_max_distance: Optional[int] = None,
         context_features: Optional[int] = None,
     ):
         super().__init__()
@@ -459,10 +427,7 @@ class TransformerBlock(nn.Module):
         self.attention = Attention(
             features=features,
             num_heads=num_heads,
-            head_features=head_features,
-            use_rel_pos=use_rel_pos,
-            rel_pos_num_buckets=rel_pos_num_buckets,
-            rel_pos_max_distance=rel_pos_max_distance,
+            head_features=head_features
         )
 
         if self.use_cross_attention:
@@ -470,18 +435,15 @@ class TransformerBlock(nn.Module):
                 features=features,
                 num_heads=num_heads,
                 head_features=head_features,
-                context_features=context_features,
-                use_rel_pos=use_rel_pos,
-                rel_pos_num_buckets=rel_pos_num_buckets,
-                rel_pos_max_distance=rel_pos_max_distance,
+                context_features=context_features
             )
 
         self.feed_forward = FeedForward(features=features, multiplier=multiplier)
 
-    def forward(self, x: Tensor, *, context: Optional[Tensor] = None) -> Tensor:
+    def forward(self, x: Tensor, *, context: Optional[Tensor] = None, context_mask: Optional[Tensor] = None) -> Tensor:
         x = self.attention(x) + x
         if self.use_cross_attention:
-            x = self.cross_attention(x, context=context) + x
+            x = self.cross_attention(x, context=context, context_mask=context_mask) + x
         x = self.feed_forward(x) + x
         return x
 
@@ -499,9 +461,6 @@ class Transformer1d(nn.Module):
         num_heads: int,
         head_features: int,
         multiplier: int,
-        use_rel_pos: bool = False,
-        rel_pos_num_buckets: Optional[int] = None,
-        rel_pos_max_distance: Optional[int] = None,
         context_features: Optional[int] = None,
     ):
         super().__init__()
@@ -524,9 +483,6 @@ class Transformer1d(nn.Module):
                     num_heads=num_heads,
                     multiplier=multiplier,
                     context_features=context_features,
-                    use_rel_pos=use_rel_pos,
-                    rel_pos_num_buckets=rel_pos_num_buckets,
-                    rel_pos_max_distance=rel_pos_max_distance,
                 )
                 for i in range(num_layers)
             ]
@@ -541,10 +497,10 @@ class Transformer1d(nn.Module):
             ),
         )
 
-    def forward(self, x: Tensor, *, context: Optional[Tensor] = None) -> Tensor:
+    def forward(self, x: Tensor, *, context: Optional[Tensor] = None, context_mask: Optional[Tensor] = None) -> Tensor:
         x = self.to_in(x)
         for block in self.blocks:
-            x = block(x, context=context)
+            x = block(x, context=context, context_mask=context_mask)
         x = self.to_out(x)
         return x
 
@@ -615,9 +571,6 @@ class DownsampleBlock1d(nn.Module):
         attention_heads: Optional[int] = None,
         attention_features: Optional[int] = None,
         attention_multiplier: Optional[int] = None,
-        attention_use_rel_pos: Optional[bool] = None,
-        attention_rel_pos_max_distance: Optional[int] = None,
-        attention_rel_pos_num_buckets: Optional[int] = None,
         context_mapping_features: Optional[int] = None,
         context_embedding_features: Optional[int] = None,
     ):
@@ -655,7 +608,6 @@ class DownsampleBlock1d(nn.Module):
                 exists(attention_heads)
                 and exists(attention_features)
                 and exists(attention_multiplier)
-                and exists(attention_use_rel_pos)
             )
             self.transformer = Transformer1d(
                 num_layers=num_transformer_blocks,
@@ -663,10 +615,7 @@ class DownsampleBlock1d(nn.Module):
                 num_heads=attention_heads,
                 head_features=attention_features,
                 multiplier=attention_multiplier,
-                context_features=context_embedding_features,
-                use_rel_pos=attention_use_rel_pos,
-                rel_pos_num_buckets=attention_rel_pos_num_buckets,
-                rel_pos_max_distance=attention_rel_pos_max_distance,
+                context_features=context_embedding_features
             )
 
         if self.use_extract:
@@ -685,6 +634,7 @@ class DownsampleBlock1d(nn.Module):
         mapping: Optional[Tensor] = None,
         channels: Optional[Tensor] = None,
         embedding: Optional[Tensor] = None,
+        embedding_mask: Optional[Tensor] = None,
     ) -> Union[Tuple[Tensor, List[Tensor]], Tensor]:
 
         if self.use_pre_downsample:
@@ -699,7 +649,7 @@ class DownsampleBlock1d(nn.Module):
             skips += [x] if self.use_skip else []
 
         if self.use_transformer:
-            x = self.transformer(x, context=embedding)
+            x = self.transformer(x, context=embedding, context_mask=embedding_mask)
             skips += [x] if self.use_skip else []
 
         if not self.use_pre_downsample:
@@ -732,9 +682,6 @@ class UpsampleBlock1d(nn.Module):
         attention_heads: Optional[int] = None,
         attention_features: Optional[int] = None,
         attention_multiplier: Optional[int] = None,
-        attention_use_rel_pos: Optional[bool] = None,
-        attention_rel_pos_max_distance: Optional[int] = None,
-        attention_rel_pos_num_buckets: Optional[int] = None,
         context_mapping_features: Optional[int] = None,
         context_embedding_features: Optional[int] = None,
     ):
@@ -766,7 +713,6 @@ class UpsampleBlock1d(nn.Module):
                 exists(attention_heads)
                 and exists(attention_features)
                 and exists(attention_multiplier)
-                and exists(attention_use_rel_pos)
             )
             self.transformer = Transformer1d(
                 num_layers=num_transformer_blocks,
@@ -775,9 +721,6 @@ class UpsampleBlock1d(nn.Module):
                 head_features=attention_features,
                 multiplier=attention_multiplier,
                 context_features=context_embedding_features,
-                use_rel_pos=attention_use_rel_pos,
-                rel_pos_num_buckets=attention_rel_pos_num_buckets,
-                rel_pos_max_distance=attention_rel_pos_max_distance,
             )
 
         self.upsample = Upsample1d(
@@ -806,6 +749,7 @@ class UpsampleBlock1d(nn.Module):
         skips: Optional[List[Tensor]] = None,
         mapping: Optional[Tensor] = None,
         embedding: Optional[Tensor] = None,
+        embedding_mask: Optional[Tensor] = None,
     ) -> Union[Tuple[Tensor, Tensor], Tensor]:
 
         if self.use_pre_upsample:
@@ -816,7 +760,7 @@ class UpsampleBlock1d(nn.Module):
             x = block(x, mapping=mapping)
 
         if self.use_transformer:
-            x = self.transformer(x, context=embedding)
+            x = self.transformer(x, context=embedding, context_mask=embedding_mask)
 
         if not self.use_pre_upsample:
             x = self.upsample(x)
@@ -838,9 +782,6 @@ class BottleneckBlock1d(nn.Module):
         attention_heads: Optional[int] = None,
         attention_features: Optional[int] = None,
         attention_multiplier: Optional[int] = None,
-        attention_use_rel_pos: Optional[bool] = None,
-        attention_rel_pos_max_distance: Optional[int] = None,
-        attention_rel_pos_num_buckets: Optional[int] = None,
         context_mapping_features: Optional[int] = None,
         context_embedding_features: Optional[int] = None,
         use_snake: bool = False,
@@ -861,7 +802,6 @@ class BottleneckBlock1d(nn.Module):
                 exists(attention_heads)
                 and exists(attention_features)
                 and exists(attention_multiplier)
-                and exists(attention_use_rel_pos)
             )
             self.transformer = Transformer1d(
                 num_layers=num_transformer_blocks,
@@ -870,9 +810,6 @@ class BottleneckBlock1d(nn.Module):
                 head_features=attention_features,
                 multiplier=attention_multiplier,
                 context_features=context_embedding_features,
-                use_rel_pos=attention_use_rel_pos,
-                rel_pos_num_buckets=attention_rel_pos_num_buckets,
-                rel_pos_max_distance=attention_rel_pos_max_distance,
             )
 
         self.post_block = ResnetBlock1d(
@@ -889,10 +826,11 @@ class BottleneckBlock1d(nn.Module):
         *,
         mapping: Optional[Tensor] = None,
         embedding: Optional[Tensor] = None,
+        embedding_mask: Optional[Tensor] = None,
     ) -> Tensor:
         x = self.pre_block(x, mapping=mapping)
         if self.use_transformer:
-            x = self.transformer(x, context=embedding)
+            x = self.transformer(x, context=embedding, context_mask=embedding_mask)
         x = self.post_block(x, mapping=mapping)
         return x
 
@@ -1123,6 +1061,7 @@ class UNet1d(nn.Module):
         features: Optional[Tensor] = None,
         channels_list: Optional[Sequence[Tensor]] = None,
         embedding: Optional[Tensor] = None,
+        embedding_mask: Optional[Tensor] = None,
     ) -> Tensor:
         channels = self.get_channels(channels_list, layer=0)
         # Apply stft if required
@@ -1137,15 +1076,15 @@ class UNet1d(nn.Module):
         for i, downsample in enumerate(self.downsamples):
             channels = self.get_channels(channels_list, layer=i + 1)
             x, skips = downsample(
-                x, mapping=mapping, channels=channels, embedding=embedding
+                x, mapping=mapping, channels=channels, embedding=embedding, embedding_mask=embedding_mask
             )
             skips_list += [skips]
 
-        x = self.bottleneck(x, mapping=mapping, embedding=embedding)
+        x = self.bottleneck(x, mapping=mapping, embedding=embedding, embedding_mask=embedding_mask)
 
         for i, upsample in enumerate(self.upsamples):
             skips = skips_list.pop()
-            x = upsample(x, skips=skips, mapping=mapping, embedding=embedding)
+            x = upsample(x, skips=skips, mapping=mapping, embedding=embedding, embedding_mask=embedding_mask)
 
         x += skips_list.pop()
         x = self.to_out(x, mapping)
@@ -1205,6 +1144,7 @@ class UNetCFG1d(UNet1d):
         time: Tensor,
         *,
         embedding: Tensor,
+        embedding_mask: Optional[Tensor] = None,
         embedding_scale: float = 1.0,
         embedding_mask_proba: float = 0.0,
         **kwargs,
@@ -1221,12 +1161,12 @@ class UNetCFG1d(UNet1d):
 
         if embedding_scale != 1.0:
             # Compute both normal and fixed embedding outputs
-            out = super().forward(x, time, embedding=embedding, **kwargs)
-            out_masked = super().forward(x, time, embedding=fixed_embedding, **kwargs)
+            out = super().forward(x, time, embedding=embedding, embedding_mask=embedding_mask, **kwargs)
+            out_masked = super().forward(x, time, embedding=fixed_embedding, embedding_mask=embedding_mask, **kwargs)
             # Scale conditional output using classifier-free guidance
             return out_masked + (out - out_masked) * embedding_scale
         else:
-            return super().forward(x, time, embedding=embedding, **kwargs)
+            return super().forward(x, time, embedding=embedding, embedding_mask=embedding_mask, **kwargs)
 
 
 class UNetNCCA1d(UNet1d):
